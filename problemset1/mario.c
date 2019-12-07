21acbae11c480aac4e09fb7acfc1f563574a6737
Antkeo1 <sleav143@gmail.com>
2019-12-07 13:38:48 -0500

    completed the mario.c successfully

--- problemset1/mario.c
+++ problemset1/mario.c
@@ -5,7 +5,7 @@ int main(void)
 {   
    int h = get_int("Height between 1 and 8: ");
     int w = h + 1;
-    
+    printf("Height: %i\n", h);
     for (int j = 0; j < h; j++) 
     {
        for (int s = 0; s < h - 1 - j; s++) 
