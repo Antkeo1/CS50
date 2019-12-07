5a00d48e48fbd1938670598b6270ac41f9bc1b87
Antkeo1 <sleav143@gmail.com>
2019-12-07 13:56:44 -0500

    fixed bug

--- problemset1/mario.c
+++ problemset1/mario.c
@@ -3,16 +3,22 @@
 
 int main(void)
 {   
-   int h = get_int("Height between 1 and 8: ");
-    int w = h + 1;
-    printf("Height: %i\n", h);
+    int h;
+   do 
+   {
+       h = get_int("Height between 1 and 8: ");
+    }
+    while (h < 0 || h > 8);
+         
+    
     for (int j = 0; j < h; j++) 
     {
        for (int s = 0; s < h - 1 - j; s++) 
        {
            printf(" ");
        }
-        for (int k = 0; k < j + 1; k++) 
+        
+        for (int k = 0; k < j + 2; k++) 
         {
              printf("#"); 
         }
@@ -20,4 +26,8 @@ int main(void)
       printf("\n");
        
     }
+   
 }
+
+
+
